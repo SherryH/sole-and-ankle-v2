6c15fa795c54dc7e419b978e9336c5b4c3efd24e
SherryH <enjoy.wind7rain@gmail.com>
2025-04-13 13:40:12 +0800

    Completed  Shoe Grid 5a

--- src/components/ShoeGrid/ShoeGrid.jsx
+++ src/components/ShoeGrid/ShoeGrid.jsx
@@ -4,16 +4,33 @@ import styled from 'styled-components';
 import SHOES from '../../data';
 import ShoeCard from '../ShoeCard';
 
+// better to separate the spacing concern from the card design
+// to preserve separation of concerns
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  /* gap: 2em; */
+  /** if not using gap, we can use margin. -ve margin here to match heading alignment */
+  display: flex;
+  flex-wrap: wrap;
+  margin: -16px;
+`;
+
+const ShoeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  flex-basis: 250px;
+`;
 
 export default ShoeGrid;
