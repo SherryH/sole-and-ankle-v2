350a8cabecd02cda3845f2fb70697b2dc7691d44
SherryH <enjoy.wind7rain@gmail.com>
2025-04-13 10:40:48 +0800

    Finished Framing

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -25,15 +25,13 @@ const ShoeIndex = ({ sortId, setSortId }) => {
           </Select>
         </Header>
         <Spacer size={34} />
-        <ShoeGrid />
+        {/* <ShoeGrid /> */}
       </MainColumn>
       <LeftColumn>
         <Breadcrumbs>
           <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
           <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
+          <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
         </Breadcrumbs>
         <Spacer size={42} />
         <ShoeSidebar />
@@ -42,17 +40,35 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse; // instead of moving LeftColumn DOM up, so that tabbing order reaches Main Column first
+  align-items: baseline; // align the text inside the flex children, visually pleasing
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  margin-right: 2em;
+  /* width: 248px; */ // use min-width or flex-basis!!
+  flex-basis: 248px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  width: 100vw;
+  flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  align-items: baseline; //
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  flex: 1;
 `;
 
 export default ShoeIndex;
